c1daa0a58515984dde8b82178dc125fe64261666
Alisa <Alisa@MacBook-Pro.local>
2024-04-11 09:35:07 +0300

    deployed

--- bookstore/src/main/java/s24/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/s24/bookstore/BookstoreApplication.java
@@ -42,8 +42,8 @@ public class BookstoreApplication {
 
 			AppUser user1 = new AppUser("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "user@email.com");
 			AppUser user2 = new AppUser("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN", "admin@mail.com");
-			urepository.save(user1);
-			urepository.save(user2);
+			//urepository.save(user1);
+			//urepository.save(user2);
 
 			log.info("fetch all books");
 			for (Book book : brepository.findAll()) {
